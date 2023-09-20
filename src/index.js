e38445a77dcd1ac8e6021967d60f1b09d437799a
Mari-glo <marigloria@gmail.com>
2023-09-20 15:08:56 -0300

    pequeño avance

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import  express from 'express';
 import { engine } from "express-handlebars";
 import { __dirname } from './utils.js';
+import * as path from "path" ;
 
 const app = express();
 const PORT = 3030;
 
 app.engine("handlebars", engine());
 app.set('view engine', "handlebars");
-app.set('')
+app.set('views', path.resolve(__dirname + "/views"));
+
+app.use('/', express.static(__dirname + "/public"));
 
 app.get('/', (req, res) =>{
     res.render("index");
